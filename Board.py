ec10eb7f539db9ab9d6718ca779284bf5d533bd4
Anna Foltyniewicz <foltyniewicz.anna@gmail.com>
2021-04-16 12:03:19 +0200

    Dodano usuwanie pionków przeciwnika

--- Board.py
+++ Board.py
@@ -5,6 +5,8 @@ class Board:
     player1 = Player
     player2 = Player
     players=[]
+    start = 0
+
     #Konstruktor tworzący planszę 6x6 wypełnioną zerami
     def __init__(self):
         self.board = [[0 for j in range(6)] for i in range(6)]
@@ -12,6 +14,7 @@ class Board:
         self.player1 = Player.Player(1)
         self.player2 = Player.Player(2)
         self.players = [self.player1,self.player2]
+        self.start=0
 
     #Wypisuje planszę
     def Printing_board(self):
@@ -26,34 +29,76 @@ class Board:
     def Four_field(self):
         if(self.board[2][2]==0 or self.board[2][3]==0 or self.board[3][2]==0 or self.board[3][3]==0):
             return False
-        else: 
+        else:
+            self.start=1 
             return True
     #Podaje ilość pionków postawionych przez gracza na planszy
     def get_players_placed_pawns(self,id):
         return self.players[id-1].placed_pawns
 
-       
 
+    #po postawieniu pionka sprawdzić czy powstała trójka
+    def if_three_pawns(self,x,y):
+
+        #Ify zapobiegają wyjściu poza zakres tablicy
+        if(x<=3):
+             if((self.board[x][y]==self.board[x+1][y]==self.board[x+2][y])and self.board[x][y]!=0):
+                self.take_off_pawn()
+                return 
+        if(x>=2):
+             if((self.board[x][y]==self.board[x-1][y]==self.board[x-2][y])and self.board[x][y]!=0):  
+                self.take_off_pawn()
+                return 
+        if(y<=3):      
+            if((self.board[x][y]==self.board[x][y+1]==self.board[x][y+2])and self.board[x][y]!=0):  
+                self.take_off_pawn()
+                return     
+        if(y>=2):    
+             if((self.board[x][y]==self.board[x][y-1]==self.board[x][y-2])and self.board[x][y]!=0):  
+                self.take_off_pawn()  
+                return                         
+                          
+                    
+    #Zdejmuje podany pionek przeciwnika
+    def take_off_pawn(self): 
+        self.Printing_board()
+        print("Removing opponent pawn")   
+        x = int(input("Enter the row number: "))
+        y = int(input("Enter the column number: "))       
+        if((self.turn==1 and self.board[x][y]==2)or(self.turn==2 and self.board[x][y]==1)):
+            self.board[x][y]=0
+        elif((self.turn==1 and self.board[x][y]==1)or(self.turn==2 and self.board[x][y]==2)): 
+            print("You can't remove your own pawn")
+            self.take_off_pawn()
+        else:
+            print("There is no pawn to remove")
+            self.take_off_pawn()
+          
     #1 - gracz 1
     #2 - gracz 2
 
     #Wstawia pionek na podane pole (x i y = od 0 do 5), jako dany gracz (1/2)
     def Put_pawn(self,x,y,player):
+        #Sprawdza czy cztery środkowe pola zostały zapełnione po raz pierwszy0
+        if(self.start==0):
+            self.Four_field()
         #Sprawdza czy zostały najpierw zajęte cztery środkowe pola oraz czy typowane pole jest puste
-        if(self.Four_field()==True and self.board[x][y]==0):
+        if(self.start==1 and self.board[x][y]==0):
             self.board[x][y]=player
             self.players[self.turn-1].placed_pawns+=1
             self.players[self.turn-1].pawns_on_board+=1
+            self.if_three_pawns(x,y)
             #Zmienia kolejkę gracza
             if(self.turn==1):
                 self.turn = 2
             else: 
                 self.turn = 1
         #Sprawdza czy gracz chce zajęć któreś z czterech środkowych pól oraz czy typowane pole jest puste
-        elif (((x==2 and (y==2 or y==3)) or (x==3 and (y==2 or y==3)))and self.board[x][y]==0):
+        elif (((x==2 and (y==2 or y==3)) or (x==3 and (y==2 or y==3)))and self.board[x][y]==0 ):
             self.board[x][y]=player
             self.players[self.turn-1].placed_pawns+=1
             self.players[self.turn-1].pawns_on_board+=1
+            self.if_three_pawns(x,y)
             #Zmienia kolejkę gracza
             if(self.turn==1):
                 self.turn = 2
--- main.py
+++ main.py
@@ -8,16 +8,18 @@ new_board = Board.Board()
 new_board.Printing_board()
 #Sprawdza czy gracze umieścili już wszystkie swoje pionki
 while (new_board.get_players_placed_pawns(1)!=12 or new_board.get_players_placed_pawns(2)!=12):
-    #Podawanie miejsca postawienia pionka
-    x = input("Podaj numer kolumny: ")
-    y = input("Podaj numer wiersza: ")
-    #Sprawdzanie kolejności gracza
-    if(new_board.turn==1):
-        id = new_board.players[0].id()
-    else:
-        id = new_board.players[1].id()
+    try:
+        #Podawanie miejsca postawienia pionka
+        x = input("Enter the row number: ")
+        y = input("Enter the column number: ")
+        #Sprawdzanie kolejności gracza
+        if(new_board.turn==1):
+            id = new_board.players[0].id()
+        else:
+            id = new_board.players[1].id()
 
-    #Umieszczanie pionka na planszy
-    new_board.Put_pawn(int(x),int(y),id)
-    new_board.Printing_board()
-
+        #Umieszczanie pionka na planszy
+        new_board.Put_pawn(int(x),int(y),id)
+        new_board.Printing_board()
+    except:
+        pass
